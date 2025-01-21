489dbfa166f0836ee726730570bb262d808aba20
wenxindongwork <wenxindong@google.com>
2025-01-21 00:51:43 +0000

    update examples

--- examples/multihost/ray/TPU/full_finetuning_example.py
+++ examples/multihost/ray/TPU/full_finetuning_example.py
@@ -1,7 +1,7 @@
 import ray
 from examples.example_datasets import example_datasets
 from typing import List, Any
-from kithara.dataset.utils import split_dataset
+from kithara.distributed.data import split_dataset
 
 ray.init()
 
