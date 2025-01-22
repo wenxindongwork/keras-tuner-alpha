d9d64975ddf7b5e3546bba3ffcfadeeefd72b339
wenxindongwork <wenxindong@google.com>
2025-01-21 16:17:27 -0800

    Update full_finetuning_example.py

--- examples/multihost/ray/TPU/full_finetuning_example.py
+++ examples/multihost/ray/TPU/full_finetuning_example.py
@@ -1,8 +1,26 @@
+"""Full parameter finetune a Gemma2 9B model.
+
+This script demonstrates how to:
+1. Set up a Gemma2 model for full parameter finetuning
+2. Load HuggingFace Gemma2 checkpoint
+3. Configure data loading and preprocessing
+4. Run training across TPU/GPU devices
+5. Save checkpoint to GCS periodically 
+6. Generate text using the trained model
+7. Save model in HuggingFace format to GCS
+
+This script should be run on multihost, since gemma2-9b will not fit on a single host. However, 
+you can change the model to `gemma2-2b` to run on single host. 
+
+Singlehost: python examples/singlehost/full_finetuning_example.py 
+Multihost:  python ray/submit_job.py "python3 examples/multihost/ray/TPU/full_finetuning_example.py" --hf-token <TOKEN>
+"""
+
 import ray
 from examples.example_datasets import example_datasets
 from typing import List, Any
 from kithara.distributed.data import split_dataset
-
+import jax 
 ray.init()
 
 # Verify TPU resources
@@ -27,12 +45,14 @@ def main(train_ds, eval_ds, dataset_is_sharded_per_host):
     import sys
     maxtext_dir = "maxtext/MaxText"
     sys.path.append(maxtext_dir)
-
+    
+    jax.distributed.initialize()
+    
     # Run workload in SPMD mode
     from examples.singlehost.full_finetuning_example import run_workload
     run_workload(
-        train_dataset=train_ds,
-        eval_dataset=eval_ds,
+        train_source=train_ds,
+        eval_source=eval_ds,
         dataset_is_sharded_per_host=dataset_is_sharded_per_host,
     )
 
