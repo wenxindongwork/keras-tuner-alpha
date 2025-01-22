f9c07a37dead6768a4ca5e6ac74eb6cf394f7b62
wenxindongwork <wenxindong@google.com>
2025-01-21 16:51:46 -0800

    update dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         # MaxText dependencies
         "aqtp",
         "grain-nightly",
-        "orbax-checkpoint",
+        "orbax-checkpoint>=0.10.3"
         "google-cloud-logging",
         "tensorboardx",
         "ml-collections",
