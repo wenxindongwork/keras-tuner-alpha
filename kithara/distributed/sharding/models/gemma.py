58f9c6feaa05138ebab93d5ee68359fe1a34b14e
wenxindongwork <wenxindong@google.com>
2025-02-10 21:06:25 +0000

    WIP

--- kithara/distributed/sharding/models/gemma.py
+++ kithara/distributed/sharding/models/gemma.py
@@ -17,15 +17,15 @@
 from kithara.distributed.sharding._mesh import Axis
 
 GEMMA_FSDP = {
-    ".*token_embedding.embeddings.*": (None, Axis.FSDP),
-    ".*decoder_block.*attention.*(query|key|value).kernel.*": (
+    ".*token_embedding.embeddings$": (None, Axis.FSDP),
+    ".*decoder_block.*attention.*(query|key|value).kernel$": (
         None,
         Axis.FSDP,
     ),
-    ".*decoder_block.*attention_output.kernel.*": (None, None, Axis.FSDP),
-    ".*decoder_block.*ffw_gating.kernel.*": (None, Axis.FSDP),
-    ".*decoder_block.*ffw_gating_2.kernel.*": (None, Axis.FSDP),
-    ".*decoder_block.*ffw_linear.kernel.*": (Axis.FSDP, None),
+    ".*decoder_block.*attention_output.kernel$": (None, None, Axis.FSDP),
+    ".*decoder_block.*ffw_gating.kernel$": (None, Axis.FSDP),
+    ".*decoder_block.*ffw_gating_2.kernel$": (None, Axis.FSDP),
+    ".*decoder_block.*ffw_linear.kernel$": (Axis.FSDP, None),
     # Lora layers
     ".*decoder_block.*attention.*(query|key|value).lora_kernel.*": (
         None,
